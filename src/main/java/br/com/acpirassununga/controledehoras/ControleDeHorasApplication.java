39c88363a3185af7d56db258c9b0ab07b452f7a8
FelipeCZ <FelipeCZ@users.noreply.github.com>
2021-11-28 22:03:31 -0300

    project from start.spring.io
    Created new initial project from start.pring.io

---
+++ src/main/java/br/com/acpirassununga/controledehoras/ControleDeHorasApplication.java
@@ -0,0 +1,13 @@
+package br.com.acpirassununga.controledehoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeHorasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/acpirassununga/controledehoras/ControleDeHorasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acpirassununga.controledehoras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeHorasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
